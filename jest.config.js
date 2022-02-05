087c1b9b4079f47b431f76b130b88ed0d86905eb
unicop <its.op.the.unicorn@gmail.com>
2022-02-05 17:56:25 +0200

    Setup jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.test.json',
+    },
+  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  clearMocks: true,
+}
