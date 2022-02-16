6ad0643bf793cb36cbc6067e50dcc7d3c9ebe5c2
unicop <its.op.the.unicorn@gmail.com>
2022-02-16 09:19:36 +0200

    test: skip collection:create test

--- test/commands/collection/create.test.ts
+++ test/commands/collection/create.test.ts
@@ -2,14 +2,15 @@ import { expect, test } from '@oclif/test'
 
 jest.setTimeout(30_000)
 
-describe.only('collection.create', () => {
+describe.skip('collection.create', () => {
   test
   .stdout()
   .command([
     'collection:create',
     // 'create',
     '-w=.pmac/workspaces/personal/MVP_id_f5dab15c-f3cd-4bb7-8e23-5d39218f7074/',
-    '-o=pokemon.openapi.yml',
+    // eslint-disable-next-line unicorn/prefer-module
+    `-o=${__dirname}/pokemon.openapi.yml`,
   ], { reset: true })
   .it('runs hello cmd', ctx => {
     console.log('sd')
